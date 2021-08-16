d56ff052fdeca74559c444d3028bc2e704d131e8
Isabell Lee <izzy.isabell@gmail.com>
2021-08-16 16:40:43 -0700

    Made progress on VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,15 +1,14 @@
 // import App from './components/App.js';
 import exampleVideoData from '/src/data/exampleVideoData.js';
-import VideoListEntry from 'VideoListEntry.jsx';
+import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>{exampleVideoData[0].snippet.title}</em><img src={exampleVideoData[0].snippet.thumbnails.default.url}/></h5></div>
-    <div><h5><em>{exampleVideoData[1].snippet.title}</em> <img src={exampleVideoData[1].snippet.thumbnails.default.url}/></h5></div>
-    <div><h5><em>{exampleVideoData[2].snippet.title}</em> <img src={exampleVideoData[2].snippet.thumbnails.default.url}/></h5></div>
-    <div><h5><em>{exampleVideoData[3].snippet.title}</em> <img src={exampleVideoData[3].snippet.thumbnails.default.url}/></h5></div>
-    <div><h5><em>{exampleVideoData[4].snippet.title}</em> <img src={exampleVideoData[4].snippet.thumbnails.default.url}/></h5></div>
-    <VideoListEntry />
+  <div className="video-list" >
+    <VideoListEntry url={exampleVideoData[0].snippet.thumbnails.default.url} title={exampleVideoData[0].snippet.title} description={exampleVideoData[0].snippet.description} />
+    <VideoListEntry url={exampleVideoData[1].snippet.thumbnails.default.url} title={exampleVideoData[1].snippet.title} description={exampleVideoData[1].snippet.description} />
+    <VideoListEntry url={exampleVideoData[2].snippet.thumbnails.default.url} title={exampleVideoData[2].snippet.title} description={exampleVideoData[2].snippet.description} />
+    <VideoListEntry url={exampleVideoData[3].snippet.thumbnails.default.url} title={exampleVideoData[3].snippet.title} description={exampleVideoData[3].snippet.description} />
+    <VideoListEntry url={exampleVideoData[4].snippet.thumbnails.default.url} title={exampleVideoData[4].snippet.title} description={exampleVideoData[4].snippet.description} />
   </div>
 );
 
