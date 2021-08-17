bf5995db0bdfd1aa15dd7e6fdebd7229a3f2f3c5
Isabell Lee <izzy.isabell@gmail.com>
2021-08-16 21:32:59 -0700

    Refactored App.jsx into ES6 class based component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,30 @@
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>Video List</em><VideoList /></h5></div>
-      </div>
-    </div>
-  </div>
-);
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em><VideoPlayer /></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>Video List</em><VideoList /></h5></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default App;
